2463d86e40e1289eaca4c7593a42a9d7409870ef
Fz-Umar <umarofficemail@gmail.com>
2024-03-07 15:59:13 +0530

    Modified Dist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
-import Signup from "./Signup"
-import Login from "./Login"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./Home"
-function App() {
- 
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Signup from "./Signup";
+import Login from "./Login";
+import Home from "./Home";
 
+function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/register" element={<Signup />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/home" element={<Home />}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/register" />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
+
